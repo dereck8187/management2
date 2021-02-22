b33f732410781169c5c10d7f40a10576a2ec18f5
viktor vaughn <dereck8187@gmail.com>
2021-02-22 00:33:01 -0500

    working

--- Develop/app.js
+++ Develop/app.js
@@ -185,6 +185,8 @@ function buildTeam() {
 
     
 }
+
+createTeam();
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
